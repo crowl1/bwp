392aab52dc3e8283e3fb51850e7fef2f2c1c8cdd
crowl1 <47753606+crowl1@users.noreply.github.com>
2020-02-25 21:55:21 +0200

    Initial commit

---
+++ BWP/Controllers/SecondController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BWP.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult AD(string name, int numTimes)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
